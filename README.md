726b80f5453b8ceaa569ceaaa2886bfae4249a70
ElenaS1981 <elenasidyakina@gmail.com>
2022-12-10 11:47:41 -0800

    Added print task method

--- README.md
+++ README.md
@@ -10,4 +10,10 @@ Inheritance is used to create a hierarchy of classes. The Command class is defin
 
 Polymorphism is used to allow for different subclasses of Command to be treated in the same way. In the TodoList class, the executeCommand method takes a Command object as a parameter, and calls the execute method on that object. This method can be called with any object that is an instance of a Command subclass, and the appropriate execute method will be called on that object, depending on its specific type. This allows for a single method to handle different types of commands in a flexible and extensible way.
 
+the Command class represents an abstract base class for commands that can be executed on a TodoList object. It defines an execute method that must be implemented by concrete subclasses, and it also holds a reference to a TodoList object.
 
+
+
+The Command class is part of an implementation of the Command pattern, which is a design pattern used to separate the responsibilities of an object into distinct, independent components. In this case, the Command class represents the "command" part of the pattern, which defines the interface for executing an action on a TodoList object. The concrete subclasses of Command (such as AddTaskCommand and RemoveTaskCommand) represent the specific actions that can be performed on a TodoList.
+
+In the TodoList class, the executeCommand method takes a Command object as a parameter and calls the execute method on that object. This allows for different actions to be performed on a TodoList object by passing different Command objects to the executeCommand method. This separation of concerns makes the TodoList class more modular and easier to extend and maintain.
--- src/Task.java
+++ src/Task.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+
+
 
 class Task {
     private String description;
@@ -24,102 +27,47 @@ class Task {
     }
 }
 
-
-
-
-abstract class Command {
-    protected TodoList todoList;
-
-    public Command(TodoList todoList) {
-        this.todoList = todoList;
-    }
-
-    public abstract void execute();
-
-
-}
-
-
-
-
-class AddTaskCommand extends Command {
-    private Task task;
-
-    public AddTaskCommand(TodoList todoList, Task task) {
-        super(todoList);
-        this.task = task;
-    }
-
-    @Override
-    public void execute() {
-        this.todoList.addTask(this.task);
-    }
-}
-
-
-
-class RemoveTaskCommand extends Command {
-    private Task task;
-
-    public RemoveTaskCommand(TodoList todoList, Task task) {
-        super(todoList);
-        this.task = task;
-    }
-
-    @Override
-    public void execute() {
-        this.todoList.removeTask(this.task);
-    }
-}
-
-
-
 class TodoList {
-    private List<Task> tasks;
-    private List<Command> commands;
-    private int currentCommandIndex;
+    private LinkedList<Task> tasks;
+    private LinkedList<Task> completedTasks;
 
     public TodoList() {
-        this.tasks = new ArrayList<>();
-        this.commands = new ArrayList<>();
-        this.currentCommandIndex = -1;
+        this.tasks = new LinkedList<>();
+        this.completedTasks = new LinkedList<>();
     }
 
     public void addTask(Task task) {
         this.tasks.add(task);
-        Command command = new AddTaskCommand(this, task);
-        this.executeCommand(command);
+
     }
 
     public void removeTask(Task task) {
         this.tasks.remove(task);
-        Command command = new RemoveTaskCommand(this, task);
-        this.executeCommand(command);
+        completedTasks.add(task);
+
     }
 
     public List<Task> getTasks() {
         return Collections.unmodifiableList(this.tasks);
     }
 
-    private void executeCommand(Command command) {
-        this.commands.add(command);
-        this.currentCommandIndex++;
-        command.execute();
-    }
 
-    public void undo() {
-        if (this.currentCommandIndex >= 0) {
-            Command command = this.commands.get(this.currentCommandIndex);
-            this.currentCommandIndex--;
-            command.undo();
+
+
+public void printCompletedTasks() {
+        for (int i = 0; i < completedTasks.size(); i++) {
+            System.out.println("Completed task:" + i + 1 + " . " + completedTasks.get(i));
         }
+
     }
 
-    public void redo() {
-        if (this.currentCommandIndex < this.commands.size() - 1) {
-            this.currentCommandIndex++;
-            Command command = this.commands.get(this.currentCommandIndex);
-            command.execute();
+
+
+
+    // print al tasks method
+    public void printAllTasks() {
+        for (Task task : this.tasks) {
+            System.out.println("Here are all the tasks: "+ task.getDescription());
         }
     }
 }
